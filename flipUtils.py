516977ff2bd7ed0186ac8333e7a73714cb2ea826
nietaki <nietaki@gmail.com>
2016-03-21 22:27:21 +0000

    communication via RS485 (baud rate 4800) sniffed and dumped

---
+++ flipUtils.py
@@ -0,0 +1,12 @@
+
+def readWell(port, length, chunk_size = 100):
+    print('clearing input without using the clear reset_input_buffer() function')
+    while port.inWaiting() > 0:
+        cleared = port.read(port.inWaiting())
+        print('cleared ' + str(len(cleared)))
+
+    ret = ''
+    while len(ret) < length:
+        ret = ret + port.read(chunk_size)
+        print(len(ret))
+    return ret
